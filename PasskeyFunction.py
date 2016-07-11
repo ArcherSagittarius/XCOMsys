09092be105dd0add51dd23a7032a7cd46b6d8757
Joseph E. Ward <eeroward@gmail.com>
2016-07-11 13:30:20 -0600

    Update PasskeyFunction.py
    This changed the raw_input to a getpass, so that the password isn't visible while typing.

--- PasskeyFunction.py
+++ PasskeyFunction.py
@@ -1,10 +1,11 @@
 import hashlib
 import sys
 import time
-
+from getpass import getpass
 #Wait Functions
-#to use a different character than the default, 
-#   just put the function and (*insert character*)
+
+#To use a different character than the default, 
+#just put the function and (*insert wanted character*)
 def waitfunc_Xshort(y='¤'):
 	for x in range(0, 9):
 		sys.stdout.write(y)
@@ -20,14 +21,18 @@ def waitfunc_long(y='¤'):
 		sys.stdout.write(y)
 		time.sleep(.07)
 		sys.stdout.flush()
-#Used for the fast methods of testing
+#Used for a we time is of the essence, usually during testing
 def waitfunc_none(y='¤'):
 	for x in range(0,9):
-        sys.stdout.write(y)
+		sys.stdout.write(y)
 		sys.stdout.flush()
-
+def correctpassword_com(s):
+	for c in s:
+		sys.stdout.write( '%s' % c )
+		sys.stdout.flush()
+		time.sleep(0.07)		
 """
---------------------------Setup imports and functions-------------------------
+-------------------Setup imports and functions--------------------
 """
 
 #Passkey varables
@@ -43,23 +48,25 @@ def run_this_function():
 	print "\nXCOM::ROSTER: Version 3.1.20.46..."
 	time.sleep(.4)
 	print "\nInitializtion complete, enter password:"
-	#Maybe make this security protection change and actually change functionalities?...
 	print "(Classified Security Protection in Force)"
 	def rerun_passwork():
 		ciao = "5f4dcc3b5aa765d61d8327deb882cf99"
-		rinput = raw_input("\nPassword: ")
-		#To quit, mearly press q or Q and enter
+		#This will not work correctly on Repl.it
+		rinput = getpass("\nPassword: ")
 		if rinput.lower() == 'q':
 			print "Exiting System..."
 			waitfunc_short()
 			print ''
 			sys.exit()
-		#If this is your 5th attempt, you've struck out or are about to
+		#This sets the number of attempts allowed 
+		#and prints if you are about to exceed that number 
 		elif rerunpassword == 5:
 			waitfunc_Xshort('-')
 			print "\nWARNING, final attempt."
 			waitfunc_Xshort('-')
 		if hashlib.md5(rinput).hexdigest() == ciao:
+			print 'Correct Password'
+			print '-' * 10
 			print "\nWelcome Commander"
 			waitfunc_short()
 			global elsebreak
@@ -71,18 +78,17 @@ def run_this_function():
 			print "EXALT hack detected..."
 			waitfunc_long()
 			time.sleep(.7)
-			print "\nProtocal 'Ash and Temples' Iniziated - Emergency Shutdown Active"
+			print "\nProtocal 'Ash and Temples' Iniziated - Emergency Shutdown Active (E.S.A.)"
 			time.sleep(.7)
 			print '\nE.S.A. complete, locking system'
 			waitfunc_long()
 			print "\nViglio Confido"
 			sys.exit()
-		
 		#Effectively a function which regulates the number of times the password can be attempted.
+		
 		else:
 			global rerunpassword
 			print "\nIncorrect Password"
-			
 			if rerunpassword < 6:
 				if True:	
 					print "Attempt (%d/5)" % rerunpassword
@@ -94,9 +100,9 @@ def run_this_function():
 				waitfunc_short()
 				print "\nLocked"
 				sys.exit()
-
 	rerun_passwork()
 run_this_function()	
 
 #Passkey encryption site: http://www.unit-conversion.info/texttools/md5/ 
 #Current Password is: password
+
